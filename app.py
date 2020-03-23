ed8b6323ca72190c388b8e58d47d1371d825c0e1
Ivan Lipko <Ivan_Lipko@epam.com>
2020-03-23 18:38:16 +0300

    add sessions

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 import vk_api
 from urllib import parse
+from uuid import uuid4
 
 app = Flask(__name__)
 
 app_id = 0
-client_secret = ''
+client_secret = '-'
 app_url = "http://justanivan.pythonanywhere.com/"
+app.secret_key = "12345ABA54321"
 
-vk_session = vk_api.VkApi(app_id=app_id, client_secret=client_secret)
-
+vk_sessions = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
+    if 'uid' not in session or session['uid'] not in vk_sessions:
+        uid = uuid4()
+        session['uid'] = uid
+        vk_session = vk_api.VkApi(app_id=app_id, client_secret=client_secret)
+        vk_sessions[uid] = vk_session
+    else:
+        vk_session = vk_sessions[session['uid']]
+
     search_result = []
     if request.method == 'POST':
         if request.form.getlist("submit")[0] == "Log in":
@@ -32,9 +41,9 @@ def home():
             search_result = [item['first_name'] + ' ' + item['last_name'] for item in search_result['items']]
 
         if request.form.getlist("submit")[0] == "Log Out":
-            vk_session.token['access_token'] = None
-            vk_session.token['user_id'] = None
-
+            del vk_sessions[session['uid']]
+            session.pop('uid', None)
+            return render_template("index.html")
 
     if vk_session.token['access_token']:
         vk = vk_session.get_api()
@@ -50,6 +59,7 @@ def home():
 
 @app.route('/auth', methods=['GET', 'POST'])
 def route_auth():
+    vk_session = vk_sessions[session['uid']]
     if request.method == "GET":
         code = request.args['code']
         redirect_url = F"{app_url}{url_for('route_auth')}"
